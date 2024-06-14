0d4b7903c7569ff0046c8497935f0e63180a8b1d
Lev Litvinov <misery7100@gmail.com>
2024-06-15 01:58:18 +0300

    :;.;.;.;.:

--- epoch_toolkit/__version__.py
+++ epoch_toolkit/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0.post15.dev0+10fbd0f"
-__version_tuple__ = (0, 0, 0, "post15", "dev0", "10fbd0f")
+__version__ = "0.1.0"
+__version_tuple__ = (0, 1, 0)
