17bbf77449a137d2faf6d0042a819029a7b3e7e5
Lev Litvinov <misery7100@gmail.com>
2024-06-15 01:10:39 +0300

    Migrate to poetry

--- epoch_toolkit/__init__.py
+++ epoch_toolkit/__init__.py
@@ -1,4 +1,4 @@
-from . import core, handler
+from . import core, handler  # plot, generator
 
 # ----------------------- #
 
---
+++ epoch_toolkit/generator/block.py
@@ -0,0 +1,2 @@
+# TODO: write definitions for EPOCH blocks
+# TODO: a proper serialization for input files with <-> transformation
